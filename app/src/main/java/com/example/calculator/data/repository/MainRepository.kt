4060bfbf5ce2f7ffd86909b36c5972c542ed9fea
Nicole G <nicolegp94@gmail.com>
2021-07-12 18:07:19 +0300

    use RPN algorithm and format to solve operations

--- app/src/main/java/com/example/calculator/data/repository/MainRepository.kt
+++ app/src/main/java/com/example/calculator/data/repository/MainRepository.kt
@@ -2,6 +2,8 @@ package com.example.calculator.data.repository
 
 import com.example.calculator.domain.model.OperationResult
 import java.lang.Double.parseDouble
+import kotlin.collections.ArrayDeque
+import kotlin.collections.ArrayList
 import kotlin.math.round
 
 class MainRepository {
@@ -11,7 +13,7 @@ class MainRepository {
     private var historyResults: ArrayList<String> = arrayListOf()
 
     private var isResult: Boolean = false
-    private var pattern = """((\d+\.?\d*)\s)?((([+*/\-])\s)?\(\s(\d+\.?\d*)(\s([+*/\-])\s(\d+\.?\d*))+\s\)\s?)?(([+*/\-])\s(\d+\.?\d*)\s?)?""".toRegex()
+    private var pattern = """((\d+\.?\d*)\s)?(([+*/-]\s)?\(\s(\d+\.?\d*)(\s[+*/-]\s(\d+\.?\d*))+\s\)\s?)?([+*/-]\s(\d+\.?\d*)\s?)?""".toRegex()
 
     fun deleteLast() {
         operation = operation.dropLast(1)
@@ -61,9 +63,10 @@ class MainRepository {
         return if (listValues.size > 1) {
             if (operation.matches(pattern)) {
                 listValues.forEach { it.trim() }
-                doOperation(listValues)
+                val aux = getRPNFormat(listValues)
+                getRPNResult(aux)
             } else {
-                OperationResult(0.0, false, "Need a number at the end")
+                OperationResult(0.0, false, "Error in the operation, repeat it")
             }
         } else {
             if (listValues.size == 1) {
@@ -78,32 +81,69 @@ class MainRepository {
         return isResult
     }
 
-    private fun doOperation(values:List<String>): OperationResult {
-        var iter = 0
-        var result = 0.0
-        while(iter < values.size-1){
-            val firstNumber = if (iter == 0) {
-                values[iter]
-            }else{
-                result.toString()
+    private fun getRPNFormat(values: List<String>): ArrayDeque<String> {
+        val operatorStack = ArrayDeque<String>()
+        val stackingOperation = ArrayDeque<String>()
+        for(token in values){
+            when {
+                verifyIfNumeric(token) -> {
+                    stackingOperation.addLast(token)
+                }
+                token == "(" -> {
+                    operatorStack.addLast(token)
+                }
+                token == ")" -> {
+                    while(operatorStack.isNotEmpty() && operatorStack.last() != "("){
+                        stackingOperation.addLast(operatorStack.removeLast())
+                    }
+                    operatorStack.removeLast()
+                }
+                else -> {
+                    while(operatorStack.isNotEmpty() && (getPreference(token) <= getPreference(operatorStack.last()))){
+                        stackingOperation.addLast(operatorStack.removeLast())
+                    }
+                    operatorStack.addLast(token)
+                }
             }
-            val operator = values[iter+1]
-            val secondNumber = values[iter+2]
-            when(operator){
-                "+" -> result = addNumbers(firstNumber, secondNumber)
-                "-" -> result = subtractNumbers(firstNumber, secondNumber)
-                "*" -> result = multiplyNumbers(firstNumber, secondNumber)
-                "/" -> result = divideNumbers(firstNumber, secondNumber)
-            }
-
-            iter += 2
         }
+        while(operatorStack.isNotEmpty() ){
+            stackingOperation.addLast(operatorStack.removeLast())
+        }
+        return stackingOperation
+    }
 
-        result = result.round(3)
-        historyOperations.add(operation)
-        historyResults.add("$result")
-        isResult = true
-        return OperationResult(result, true, "")
+    private fun getPreference(s: String): Int{
+        return if(s == "*" || s == "/"){
+            3
+        } else if (s == "+" || s == "-"){
+            2
+        }else{
+            -1
+        }
+    }
+
+    private fun getRPNResult(values: ArrayDeque<String>): OperationResult{
+        val operationPile = ArrayDeque<String>()
+        for(value in values){
+            if(verifyIfNumeric(value)){
+                operationPile.addLast(value)
+            }else{
+                val result = doOperation(operationPile.removeLast(), operationPile.removeLast(), value)
+                operationPile.addLast(result)
+            }
+        }
+        return OperationResult(operationPile.last().toDouble().round(3), true, "")
+    }
+
+    private fun doOperation(firstNumber: String, secondNumber: String, operator: String): String{
+        var result = 0.0
+        when(operator){
+            "+" -> result = addNumbers(firstNumber, secondNumber)
+            "-" -> result = subtractNumbers(firstNumber, secondNumber)
+            "*" -> result = multiplyNumbers(firstNumber, secondNumber)
+            "/" -> result = divideNumbers(secondNumber, firstNumber)
+        }
+        return result.toString()
     }
 
     private fun addNumbers(firstOperator: String, secondOperator:String): Double{
